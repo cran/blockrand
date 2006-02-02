452e54a3247ae0df54e6f3d72b049a60c1844335
Greg Snow <greg.snow@imail.org>
2006-02-02 00:00:00 +0000

    version 1.1

--- R/blockrand.R
+++ R/blockrand.R
@@ -1,56 +1,75 @@
 "blockrand" <-
 function(n, num.levels=2, levels=LETTERS[seq(length=num.levels)],
-                      id.prefix, stratum, block.sizes=1:4, block.prefix){
+                      id.prefix, stratum, block.sizes=1:4, block.prefix,
+         uneq.beg=FALSE, uneq.mid=FALSE, uneq.min=0,uneq.maxit=10){
 
-  treat <- vector(mode(levels))
-  block.id <- numeric(0)
-  block.size <- numeric(0)
+    treat <- vector(mode(levels))
+    block.id <- numeric(0)
+    block.size <- numeric(0)
 
-  i <- 1
-  while(length(treat) < n) {
-    block.n <- sample(block.sizes,1)
-    tmp <- rep(levels,block.n)
-    n2 <- length(tmp)
+    i <- 1
+    while(length(treat) < n) {
 
-    treat <- c(treat, sample(tmp))
-    block.id <- c(block.id, rep(i, n2) )
-    block.size <- c(block.size, rep(n2,n2))
+        if ( (uneq.beg && (i == 1)) ||
+                 (uneq.mid && ( length(treat) > n/2)) ) {
+            n2 <- max(block.sizes)*length(levels) + 1
+            ii <- uneq.maxit
+            while( ii > 0 ) {
+                tmp <- sample(levels, n2, replace=TRUE)
+                if ( diff(range(table(tmp))) >= uneq.min ) { ii <- 0 }
+                ii <- ii -1
+            }
+            treat <- c(treat, tmp)
+            block.id <- c( block.id, rep(i,n2) )
+            block.size <- c(block.size, rep(n2,n2))
 
-    i <- i + 1
-  }
+            i <- i + 1
+            if( length(treat) > n/2 ) { uneq.mid <- FALSE }
+        }
 
-  n3 <- length(treat)
-  if(missing(id.prefix)){
-    id <- seq(length.out=n3)
-  } else if( is.numeric(id.prefix) ){
-    id <- seq(id.prefix, length.out=n3)
-  } else {
-    w <- floor(logb(n3,10))+1
-    id <- paste(id.prefix, formatC(seq(length.out=n3),flag='0',width=w),
-                sep='')
-  }
+        block.n <- sample(block.sizes,1)
+        tmp <- rep(levels,block.n)
+        n2 <- length(tmp)
 
-  out <- data.frame(id=I(id))
+        treat <- c(treat, sample(tmp))
+        block.id <- c(block.id, rep(i, n2) )
+        block.size <- c(block.size, rep(n2,n2))
 
-  if(!missing(stratum)){
-    out$stratum <- rep(stratum,n3)
-  }
+        i <- i + 1
+    }
 
-  if(missing(block.prefix)) {
-    out$block.id <- factor(block.id)
-  } else if (is.numeric(block.prefix)) {
-    out$block.id <- factor( block.id+block.prefix )
-  } else {
-    w <- floor(logb(max(block.id),10))+1
-    out$block.id <- factor( paste(block.prefix,
-                                  formatC(block.id,flag='0',width=w),
-                                  sep='') )
-  }
+    n3 <- length(treat)
+    if(missing(id.prefix)){
+        id <- seq(length.out=n3)
+    } else if( is.numeric(id.prefix) ){
+        id <- seq(id.prefix, length.out=n3)
+    } else {
+        w <- floor(logb(n3,10))+1
+        id <- paste(id.prefix, formatC(seq(length.out=n3),flag='0',width=w),
+                    sep='')
+    }
 
-  out$block.size <- block.size
-  
-  out$treatment <- factor(treat)
+    out <- data.frame(id=I(id))
 
-  return(out)
+    if(!missing(stratum)){
+        out$stratum <- rep(stratum,n3)
+    }
+
+    if(missing(block.prefix)) {
+        out$block.id <- factor(block.id)
+    } else if (is.numeric(block.prefix)) {
+        out$block.id <- factor( block.id+block.prefix )
+    } else {
+        w <- floor(logb(max(block.id),10))+1
+        out$block.id <- factor( paste(block.prefix,
+                                      formatC(block.id,flag='0',width=w),
+                                      sep='') )
+    }
+
+    out$block.size <- block.size
+
+    out$treatment <- factor(treat)
+
+    return(out)
 }
 
